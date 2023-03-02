20a7d952d4c636628dcffb05350ea091906e1742
Sagar1079 <sagarwankhade896@gmail.com>
2023-03-02 14:07:58 +0530

    C

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
 
-const {wordRouter}=require('./routes/word.route')
+const {wordRouter}=require('./routes/word.route');
+const { userRouter } = require("./routes/user.route");
 
 const app = express();
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.use("/word",wordRouter)
+app.use("/user",userRouter)
 
 app.get('/',(req,res)=>{
   res.send("HOME")
---
+++ routes/user.route.js
@@ -0,0 +1,21 @@
+const { Router } = require("express");
+const { UserModel } = require("../models/user.model");
+
+const userRouter = Router();
+
+userRouter.post("/", async (req, res) => {
+  const { userData } = req.body;
+
+  try {
+    const newUser = UserModel({ ...userData });
+    await newUser.save();
+
+    console.log(newUser);
+    res.send("Details set Successfully");
+  } catch (err) {
+    res.send("Error");
+  }
+});
+
+module.exports = { userRouter };
+ 
